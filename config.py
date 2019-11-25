3a3d888d98071e29cf126f179dde6e9b82ce0adf
Josephat-n <josphatnjoroge254@gmail.com>
2019-11-25 15:38:44 +0300

    add configuration options.

--- config.py
+++ config.py
@@ -1,1 +1,15 @@
+import os
 
+class Config:
+  pass
+
+class ProdConfig(Config):
+   pass
+
+class DevConfig(Config):
+   DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
