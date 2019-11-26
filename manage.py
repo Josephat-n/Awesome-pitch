6ff03fea6c2f942636958a9988db268949728c4d
Josephat-n <josphatnjoroge254@gmail.com>
2019-11-26 06:23:50 +0300

    create user and role tables in the db.

--- manage.py
+++ manage.py
@@ -1,5 +1,6 @@
-from app import create_app
+from app import create_app,db
 from flask_script import Manager,Server
+from app.models import User, Role
 
 
 # Creating app instance
@@ -11,6 +12,10 @@ manager.add_command('server',Server)
 @manager.command
 def test():
    pass
+
+@manager.shell
+def make_shell_context():
+    return dict(app = app,db = db,User = User, Role = Role )
 
 if __name__=='__main__':
     manager.run()
