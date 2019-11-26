5e8386819438b482f418774b64bbd2b55e253006
Josephat-n <josphatnjoroge254@gmail.com>
2019-11-26 14:54:49 +0300

    create a form to write a comment message, and make sure one is logged in to comment.

--- app/main/views.py
+++ app/main/views.py
@@ -1,8 +1,9 @@
 from flask import render_template
 from . import main
-from flask_login import login_required
+from flask_login import login_required, current_user
 from .. import db
 from ..models import Pitch
+from .forms import CommentForm
 
 #This defines the various views in our app
 @main.route('/')
@@ -24,3 +25,12 @@ def pitch(id):
    a_pitch = Pitch.query.filter_by(id= id)
    print(a_pitch)
    return render_template('pitch.html', id = id, a_pitch = a_pitch)
+
+@main.route('/pitch/comment/<int:id>', methods = ['GET','POST'])
+@login_required
+def comment(id):
+   """
+   Write a comment on any pitch
+   """
+   form = CommentForm()
+   return render_template('comment.html',id = id, comment_form=form)
