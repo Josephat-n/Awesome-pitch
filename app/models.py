7f9375ddf92f4a2d283975db3c28218371f278e8
Josephat-n <josphatnjoroge254@gmail.com>
2019-11-26 06:37:39 +0300

    import modules for securing passwords and create functions for managing the passwords.

--- app/models.py
+++ app/models.py
@@ -1,23 +1,36 @@
 from . import db
+from werkzeug.security import generate_password_hash,check_password_hash
 
 class User(db.Model):
-    __tablename__ = 'users'
+   __tablename__ = 'users'
 
-    id = db.Column(db.Integer,primary_key = True)
-    username = db.Column(db.String(255))
-    role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+   id = db.Column(db.Integer,primary_key = True)
+   username = db.Column(db.String(255))
+   role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+   
+   @property
+   def password(self):
+      raise AttributeError('You cannot read the password attribute')
+
+   @password.setter
+   def password(self, password):
+      self.pass_secure = generate_password_hash(password)
 
 
-    def __repr__(self):
-      return f'User {self.username}'
+   def verify_password(self,password):
+      return check_password_hash(self.pass_secure,password)   
+
+   def __repr__(self):
+   return f'User {self.username}'
      
 class Role(db.Model):
-    __tablename__ = 'roles'
+   __tablename__ = 'roles'
 
-    id = db.Column(db.Integer,primary_key = True)
-    name = db.Column(db.String(255))
-    users = db.relationship('User',backref = 'role',lazy="dynamic")
+   id = db.Column(db.Integer,primary_key = True)
+   name = db.Column(db.String(255))
+   users = db.relationship('User',backref = 'role',lazy="dynamic")
+   pass_secure = db.Column(db.String(255))
 
-    def __repr__(self):
-        return f'User {self.name}'     
+   def __repr__(self):
+      return f'User {self.name}'     
      
