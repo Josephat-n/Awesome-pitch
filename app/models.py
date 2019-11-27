2baa8d3510040de956350b96f0083e9281d6289e
Josephat-n <josphatnjoroge254@gmail.com>
2019-11-27 08:15:31 +0300

    push to heroku.

---
+++ app/models.py
@@ -0,0 +1,56 @@
+from . import db
+from werkzeug.security import generate_password_hash,check_password_hash
+from . import login_manager
+from flask_login import UserMixin
+
+@login_manager.user_loader
+def load_user(user_id):
+   return User.query.get(int(user_id))
+
+class User(UserMixin,db.Model):
+   __tablename__ = 'users'
+
+   id = db.Column(db.Integer,primary_key = True)
+   username = db.Column(db.String(255),index = True)
+   email = db.Column(db.String(255),unique = True,index = True)
+   role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+   pass_secure = db.Column(db.String(255))
+   password_hash = db.Column(db.String(255))
+   pitches = db.relationship('Pitch',backref = 'user',lazy="dynamic")
+   
+   @property
+   def password(self):
+      raise AttributeError('You cannot read the password attribute')
+
+   @password.setter
+   def password(self, password):
+      self.pass_secure = generate_password_hash(password)
+
+
+   def verify_password(self,password):
+      return check_password_hash(self.pass_secure,password)   
+
+   def __repr__(self):
+      return f'User {self.username}'
+     
+class Role(db.Model):
+   __tablename__ = 'roles'
+
+   id = db.Column(db.Integer,primary_key = True)
+   name = db.Column(db.String(255))
+   users = db.relationship('User',backref = 'role',lazy="dynamic")
+   pass_secure = db.Column(db.String(255))
+
+   def __repr__(self):
+      return f'User {self.name}'     
+     
+class Pitch(db.Model):
+   __tablename____ = 'pitches'
+   
+   id = db.Column(db.Integer,primary_key = True)    
+   pitch_msg =  db.Column(db.String(255))
+   user_id = db.Column(db.Integer,db.ForeignKey('users.id'))
+   
+   
+   def __repr__(self):
+      return f'Pitch {self.pitch_msg}'
---
+++ config.py
@@ -0,0 +1,28 @@
+import os
+
+class Config:
+   SECRET_KEY = os.environ.get('SECRET_KEY')	 
+   
+   
+	#Email configurations
+	MAIL_SERVER = 'smtp.googlemail.com'
+	MAIL_PORT = 587
+	MAIL_USE_TLS = True
+	MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+	MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+ 
+ 	# simple mde  configurations
+	SIMPLEMDE_JS_IIFE = True
+	SIMPLEMDE_USE_CDN = True
+
+class ProdConfig(Config):
+   SQLALCHEMY_DATABASE_URI=os.environ.get("DATABASE_URI")
+
+class DevConfig(Config):
+   SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://njoro:njoro@localhost/awesome'
+   DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
